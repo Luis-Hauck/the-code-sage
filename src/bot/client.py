e6cc41d171d05c9391f0fc565e462b6766ad3981
Luis Gustavo Hauck <hauck.luis@gmail.com>
2025-08-31 15:26:50 -0300

    fix: Corrige erro de indentação do setup_hook

--- src/bot/client.py
+++ src/bot/client.py
@@ -2,7 +2,10 @@ import os
 import discord
 from discord.ext import commands
 from app.config import GUILD_ID
-from app.logging import logger
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 class TheCodeSageBot(commands.Bot):
@@ -12,36 +15,36 @@ class TheCodeSageBot(commands.Bot):
                          intents=discord.Intents.all() # Define as intents do bot
                          )
 
-        async def setup_hook(self):
-            """"O hook é cahamado automaticamente antes do bot logar"""
+    async def setup_hook(self):
+        """"O hook é cahamado automaticamente antes do bot logar"""
 
-            #Carregamos todos os Cogs da pasta cogs
-            # Percorremos cada arquivo da pasat cogs:
-            for filename in os.listdir('.bot/cogs'):
-                # Se for um arquivo python e não iniciar com __
-                if filename.endswith('.py') and not filename.startswith('__'):
-                    try:
-                        # Carregamos o cog, usamos [:-3] para retirar .py do arquivo
-                        await self.load_extension(f'bot.cogs.{filename[:-3]}')
-                        logger.info(f'"bot.cogs.{filename[:-3]}" carregado com sucesso!')
+        #Carregamos todos os Cogs da pasta cogs
+        # Percorremos cada arquivo da pasat cogs:
+        for filename in os.listdir('./src/cogs'):
+            # Se for um arquivo python e não iniciar com __
+            if filename.endswith('.py') and not filename.startswith('__'):
+                try:
+                    # Carregamos o cog, usamos [:-3] para retirar .py do arquivo
+                    await self.load_extension(f'src.cogs.{filename[:-3]}')
+                    logger.info(f'"src.cogs.{filename[:-3]}" carregado com sucesso!')
 
-                    except Exception as e:
-                        logger.warning(f'Falha ao carregar o cog {filename[:-3]}')
+                except Exception as e:
+                    logger.warning(f'Falha ao carregar o cog {filename[:-3]}')
 
-            # Sincronizamos os slaah commands com o discord
+        # Sincronizamos os slaah commands com o discord
 
-            """
-            Sincronizar globalmente:
-            
-            await self.tree.sync()
-            """
+        """
+        Sincronizar globalmente:
+        
+        await self.tree.sync()
+        """
 
-            # sincronizar no servidor de testes
-            guild_obj = discord.Object(id=GUILD_ID)
-            self.tree.copy_global_to(guild=guild_obj)
-            await self.tree.sync(guild=guild_obj)
+        # sincronizar no servidor de testes
+        guild_obj = discord.Object(id=GUILD_ID)
+        self.tree.copy_global_to(guild=guild_obj)
+        await self.tree.sync(guild=guild_obj)
 
-            logger.info(f'Hook criado com sucesso!')
+        logger.info(f'Hook criado com sucesso!')
 
 
 
