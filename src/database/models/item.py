683894e6b3ecb3e1487b17e44166284d7c927f00
Luis Gustavo Hauck <hauck.luis@gmail.com>
2025-09-20 15:41:47 -0300

    refactor(model): Melhora a estrutura de efeitos do ItemsModel
    Esta refatoração torna o sistema de itens mais flexível,.
    
    - O campo `item_type`, que era uma string, foi substituído por um `Enum ItemType`, garantindo a validação dos tipos de item permitidos.
    
    - O antigo `equip_effect` (que suportava apenas um bônus) foi trocado pelo campo `passive_effects`, uma lista que permite que um item tenha múltiplos bônus passivos.
    
    - A estrutura de todos os efeitos (consumíveis e passivos) agora utiliza o padrão de União Discriminada do Pydantic (Union + Literal), permitindo a validação robusta de diferentes formatos de efeito.

---
+++ src/database/models/item.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from enum import Enum
+from src.database.models.effects import AnyEffect, AnyPassiveEffect
+class ItemType(str, Enum):
+    """
+    Modelo com os tipos de itens disponiveis
+    """
+    CONSUMABLE = "consumível"
+    EQUIPPABLE = "equipável"
+    ROLE = "cargo"
+
+
+class ItemsModel(BaseModel):
+    """
+    Modelo de dados para os itens.
+
+    item_id (int) id único do item
+    name: Nome do item
+    description: Descrição do item
+    price: Preço do item
+    type: Tipo do item ["cargo", "item_consumivel", "badge"]
+    effect: Efeito ao usar usar o item.
+    passive_effects: Efeitos passivos que o item dá.
+
+    """
+    item_id: int = Field(alias='_id')
+    name: str
+    description: str
+    price: int
+    item_type: ItemType
+    effect: Optional[AnyEffect] = Field(None, description='type')
+    passive_effects: Optional[AnyPassiveEffect] = []
+
+    class Config:
+        populate_by_name = True
+
