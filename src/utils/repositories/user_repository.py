a3d4d26626fb4d942eb498e6fec515a878de83c7
Luis Gustavo Hauck <hauck.luis@gmail.com>
2025-10-11 19:29:45 -0300

    feat(user_repository) criação do método add_xp_coin
    Função responsável por adicionar xp e moedas ao jogador pelo ID.
    
    - tenta incrementar o xp e as moedas ao id do jogador, se coseguir retorna True, se não False

--- src/utils/repositories/user_repository.py
+++ src/utils/repositories/user_repository.py
@@ -3,14 +3,14 @@ from pymongo.synchronous.database import Database
 from pydantic import ValidationError
 import logging
 
-from src.database.models import UserModel, UserStatus
+from src.database.models.user import UserModel, UserStatus
 
 logger = logging.getLogger('__name__')
 
 class UserRepository:
     def __init__(self, db: Database):
         # Conexão com a coleção User
-        self.collection = db.user
+        self.collection = db.users
 
     async def create(self, user_model:UserModel) -> bool:
         """Cria um novo usuário"""
@@ -19,12 +19,9 @@ class UserRepository:
             await self.collection.insert_one(user_data)
             logger.info('Usuário cdastrado')
             return True
-        except ValidationError:
-            logger.warning('Erro na validação do usuário.')
-            return False
 
         except Exception as e:
-            logger.error(f'Erro ao criar o usuário: {e}')
+            logger.error(f'Erro inesperado ao criar o usuário: {e}', exc_info=True)
             return False
 
     async def update_status(self, user_id: int, status: UserStatus) -> bool:
@@ -35,11 +32,31 @@ class UserRepository:
                 {'$set': {'status': status}}
             )
             logger.info(f'Status do user {user_id} atualizado!')
-            return True
+            return result.modified_count > 0
 
         except Exception as e:
             logger.error(f'Falha ao atualizar os status do user {user_id}: {e}')
             return False
+
+    async def add_xp_coins(self, user_id:int, xp:float, coins:float):
+        try:
+            result = await self.collection.update_one(
+                {'_id': user_id},
+                {
+                    '$inc': {
+                    'xp':xp,
+                    'coins':coins
+                    }
+                }
+            )
+            if result.modified_count > 0:
+                logger.info(f'XP e moedas incrementadas para user {user_id}')
+                return True
+
+        except Exception as e:
+            logger.error(f'Falha ao incremenatar xp e moedas ao user {user_id}: {e}')
+            return False
+
 
 
 
