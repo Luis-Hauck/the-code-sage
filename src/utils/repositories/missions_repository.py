2dff58208dbd234d45e169d754bd90588a3139e9
Luis Gustavo Hauck <hauck.luis@gmail.com>
2025-11-25 20:28:07 -0300

    feat(mission_repository) criação da função update status
    Função responsável por atualizar uma missão.
    
    - Recebe o ID da missão, o novo status, e a data que foi completada.
    - Retorna True se ocorreu a atualização caso contrário, False

--- src/utils/repositories/missions_repository.py
+++ src/utils/repositories/missions_repository.py
@@ -1,7 +1,9 @@
 from pymongo.database import Database
 import logging
-from src.database.models.mission import MissionModel
+from src.database.models.mission import MissionModel, MissionStatus
 from pymongo.errors import DuplicateKeyError
+from datetime import datetime
+from typing import Optional, Dict, Any
 
 logger = logging.getLogger(__name__)
 
@@ -26,6 +28,10 @@ class MissionRepository:
 
             return True
 
+        except DuplicateKeyError:
+            logger.warning(f"Erro, tentativa de criar uma nova missão")
+            return False
+
         except Exception as e:
             logger.error(f'Erro ao criar a missão de id{mission_model.id}: {e}')
             return False
@@ -48,6 +54,40 @@ class MissionRepository:
         except Exception as e:
             logger.error(f'Erro ao buscar a missão de ID {mission_id}: {e}', exc_info=True)
             return None
+
+    async def update_status(self, mission_id:int, new_status: MissionStatus, completed_at: Optional[datetime] = None) -> bool:
+        """
+        Atualiza os status da missão.
+        :param mission_id: ID da missão.
+        :param new_status: Status da missão.
+        :param completed_at: data que a missão foi concluída.
+        :return: True se atualizou com sucesso, False caso contrário.
+        """
+
+        try:
+            update_data: Dict[str, Any] = {'status': new_status}
+
+            if completed_at:
+                update_data['completed_at'] = completed_at
+
+            result = self.collection.update_one(
+                {'_id':mission_id},
+                {'$set': update_data}
+            )
+
+            if result.matched_count > 0:
+                logger.info(f'A missão com ID: {mission_id} foi definida como {update_data['status']}.')
+                return True
+
+            logger.warning(f'Tentativa de atualizar uma missão inexistente: {mission_id}.')
+            return False
+
+        except Exception as e:
+            logger.info(f'Erro ao atualizar a missão com ID:{mission_id}: {e}')
+            return False
+            
+
+
 
 
 
