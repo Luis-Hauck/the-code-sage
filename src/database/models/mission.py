f361d3da097e7641578ac1d586d583907a15189a
Luis Gustavo Hauck <hauck.luis@gmail.com>
2025-11-25 21:25:42 -0300

    feat(mission_model) criação da classe EvaluationRank
    - As missões agora são divididas em ranks de A a E (e S).
    - A propriedade `score` converte o rank para nota (0-5).
    - `description` retorna a descrição do rank.
    - `color` retorna a cor em hexadecimal para embeds.
    - `from_score` converte nota numérica em rank.

--- src/database/models/mission.py
+++ src/database/models/mission.py
@@ -12,6 +12,73 @@ class MissionStatus(str, Enum):
     UNDER_REVIEW = 'sob_revisao' # missão em análise
     CLOSED = 'fechada'  # missão fechada e não pode mais er aberta
 
+class EvaluationRank(str, Enum):
+    """
+    Representa os ranks das missões
+    """
+    S = 'S'
+    A = 'A'
+    B = 'B'
+    C = 'C'
+    D = 'D'
+    E = 'E'
+
+    @property
+    def score(self) -> int:
+        """
+        :return: A nota númerica de (0 a 5) associada ao rank
+        """
+        scores = {'S':5, 'A':4, 'B':3, 'C':2, 'D':1, 'E':0}
+
+        return scores.get(self.value, 0)
+
+    @property
+    def description(self) -> str:
+        """
+        :return: A descrição dos ranks
+        """
+        descriptions ={
+            'S':'Lendário',
+            'A':'Excelente',
+            'B': 'Bom',
+            'C': 'Regular',
+            'D': 'Ruim',
+            'E': 'Falha na missão'
+
+        }
+        return descriptions.get(self.value, "Desconhecido")
+
+    @property
+    def color(self) -> int:
+        """Retorna a cor em Hex (int) para Embeds do Discord."""
+        colors = {
+            "S": 0xFFD700,  # Dourado
+            "A": 0x00FF00,  # Verde
+            "B": 0x0000FF,  # Azul
+            "C": 0xFFA500,  # Laranja
+            "D": 0xFF0000,  # Vermelho
+            "E": 0x808080   # Cinza
+        }
+
+        return colors.get(self.value, 0xFFFFFF)
+
+    @classmethod
+    def from_score(cls, score: int) -> "EvaluationRank":
+        """Converte uma nota numérica para um Rank."""
+        match score:
+            case 5:
+                return cls.S
+            case 4:
+                return cls.A
+            case 3:
+                return cls.B
+            case 2:
+                return cls.C
+            case 1:
+                return cls.D
+            case _:
+                return cls.E
+
 
 
 class EvaluatorModel(BaseModel):
