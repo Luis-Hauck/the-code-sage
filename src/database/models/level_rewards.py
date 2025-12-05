01df7cbab91918ef24bf9e5021dfd9e4c03a066f
Luis Gustavo Hauck <hauck.luis@gmail.com>
2025-12-04 21:21:17 -0300

    feat(level_rewards_models) criação do modelo de recompensa de cargos por nível
     Representa um nível de recompensa, nesse nível ganha x cargo.
        level_required: Nível requerido apra o cargo.
        role_id: ID do cargo.
        role_name: Nome do cargo.

---
+++ src/database/models/level_rewards.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class LevelRewardsModel(BaseModel):
+    """
+    Representa um nível de recompensa, nesse nível ganha x cargo.
+    level_required: Nível requerido apra o cargo.
+    role_id: ID do cargo.
+    role_name: Nome do cargo.
+    """
+
+    level_required:int
+    role_id:int
+    role_name:str
+
+    class Config:
+        populate_by_name = True
