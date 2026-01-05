619a2ba811ba9b4241c7434c726065a9aab0a28c
Luis Gustavo Hauck <hauck.luis@gmail.com>
2026-01-05 11:50:53 -0300

    feat(mission_embeds) implementa classe MissionEmbeds para visualização de missões
    Gera as visualizações que u usuário irá ver relacionado as missões.
    
    - Cria classe utilitária com métodos estáticos (@staticmethod) para gerar Embeds.
    - Implementa visuais para: Início de Missão, Sucesso na Avaliação (com cores de Rank), Reports e Erros.
    - Integra ícones e thumbnails dinâmicas baseadas no EvaluationRank.
    - Padroniza a identidade visual (cores, footers e timestamps) das respostas do bot.

--- src/utils/embeds.py
+++ src/utils/embeds.py
@@ -1,1 +1,149 @@
- 
+import discord
+from datetime import datetime
+from typing import Optional
+
+from discord.types import embed
+
+from src.database.models.mission import EvaluationRank
+
+def create_error_embed(message: str) -> discord.Embed:
+    """
+    Função gerar embeds de erro.
+    """
+    return discord.Embed(
+        description=message,
+        color=discord.Color.red()
+    )
+
+def create_info_embed(message: str) -> discord.Embed:
+    """
+    Função para avisos simples (Azul/Cinza).
+    """
+    return discord.Embed(
+        description=message,
+        color=discord.Color.blue()
+    )
+
+
+class MissionEmbeds:
+
+    @staticmethod
+    def evaluation_success(target_user:discord.Member, rank:EvaluationRank, xp:int, coins:int) -> discord.Embed:
+        """
+        Gera o Embed de avaliação
+        :param target_user: Usuário avaliado;
+        :param rank: Rank da avaliação;
+        :param xp: XP ganho;
+        :param coins: Moedas ganhas;
+        :return: Retorna a embed gerada.
+        """
+
+        embed = discord.Embed(title=f'{target_user.mention} Completou a missão!',
+                              description=f'Obrigado por contribuir com a comunidade!',
+                              color=discord.Color(rank.color)
+                              )
+
+        embed.set_thumbnail(url=rank.thumbnail_url)
+
+        embed.add_field(name='Rank', value=rank.value, inline=True)
+        embed.add_field(name='XP', value=xp, inline=True)
+        embed.add_field(name='Moedas', value=coins, inline=True)
+        embed.set_footer(text='Caso não tenha recebido sua avaliação ou acha ela foi injusta, use /review e iremos analisar!')
+
+
+        return embed
+
+    @staticmethod
+    def mission_start(riddle_text) -> discord.Embed:
+        """
+        Gera o embed de quando uma missão e criada, juntamente com um enigma para ajudar.
+        :param riddle_text:
+        :return: O embed gerado.
+        """
+
+        embed = discord.Embed(title='Missão criada!',
+                              description=(f'*{riddle_text}*\n\n'
+                                           f'> 💎 **Não esqueça de recompensar o Aventureiro que te ajudou!**\n'
+                                           f"> Ao final, use o comando: `/avaliar`\n"
+                                           f"> Isso garante **XP** e **Moedas** para quem te salvou.\n\n"
+
+                                           "Se você completar a missão sozinho, use o comando `/encerrar_missão`."
+                                           ),
+
+                              color=discord.Color.from_rgb(88, 55, 250)
+        )
+
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/1253476072553451590/1457068144194617445/ac215eaefff22d1b2a35e5a5b17c959b.gif?ex=695aa7f4&is=69595674&hm=69fc2329302bf888fb76287432c2f61b9bff11bdae2114b05e46fdc6783de7da&')
+        embed.set_footer(text="Code Sage • Transformando dúvidas em XP")
+
+        return embed
+
+    @staticmethod
+    def mission_report(mission_id: int, mission_title:str, reporter_id: int, reporter_name:str, current_rank:str, reason: str) -> embed.Embed:
+        """
+        Gera o embed de report de missões
+        :param mission_id: ID da missão;
+        :param mission_title: Título da missão reportada;
+        :param reporter_id: ID do reportador;
+        :param reporter_name: Nome do reportador;
+        :param current_rank: Rank atual do reportador;
+        :param reason: Motivo do report;
+        :return: O embed gerado.
+        """
+
+        embed = discord.Embed(title=f'Report da missão: {mission_title}',
+                              description=f'Um usuário contestou uma avaliação.',
+                              color=discord.Color.red(),
+                              timestamp=datetime.now()
+        )
+
+        embed.add_field(name='Reportado por', value=f'{reporter_name} ({reporter_id})', inline=True)
+        embed.add_field(name='ID da missão', value=mission_id, inline=True)
+        embed.add_field(name='Rank atual', value=current_rank, inline=True)
+        embed.add_field(name='Motivo do report', value=reason, inline=False)
+
+        return embed
+
+    @staticmethod
+    def admin_adjustment(target_user:discord.Member, old_rank:EvaluationRank, new_rank:EvaluationRank, xp_diff:int, coins_diff:int) -> embed.Embed:
+        """
+        Exibe o resultado de uma ajuste de rank de administrador.
+        Mostras a diferença de valores entre o antigo e o novo rank.
+        :param target_user: Usuário que recebeu o ajuste.
+        :param old_rank: Antigo rank;
+        :param new_rank: Novo rank;
+        :param xp_diff: Diferença de XP entre o antigo e o novo rank.
+        :param coins_diff: Diferença de moedas entre o antigo e o novo rank.
+        :return: Embed gerado.
+        """
+
+        embed = discord.Embed(title=f'Ajuste de rank realizado!',
+                              description=f'O rank de {target_user.mention} foi ajustado pela moderação!.',
+                              color=discord.Color(new_rank.color)
+        )
+
+        # Formata o saldo com sinal (+50 ou -50)
+        xp_str = f"+{xp_diff}" if xp_diff > 0 else f"{xp_diff}"
+        coins_str = f"+{coins_diff}" if coins_diff > 0 else f"{coins_diff}"
+
+        embed.add_field(name='Rank antigo ⬇️', value=old_rank.value, inline=True)
+        embed.add_field(name='Rank novo ⬆️', value=new_rank.value, inline=True)
+        embed.add_field(name='Diferença de XP', value=xp_str, inline=True)
+        embed.add_field(name='Diferença de Moedas', value=coins_str, inline=True)
+
+
+        return embed
+
+    @staticmethod
+    def report_confirmation() -> discord.Embed:
+        """
+        Feedback que o usuário vê ao reportar sua nota na missão.
+        :return: Embed gerado.
+        """
+        return discord.Embed(
+            title='Denúncia Enviada',
+            description="Nossa equipe de sábios moderadores irá analisar o caso.\n"
+                        "Se a nota for ajustada, você receberá a diferença de XP/Moedas automaticamente.",
+            color=discord.Color.green()
+        )
+
