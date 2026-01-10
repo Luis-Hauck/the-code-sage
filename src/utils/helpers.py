ebcec06416c515a2f66bb958853868f41746a008
Luis Gustavo Hauck <hauck.luis@gmail.com>
2026-01-10 11:19:12 -0300

    feat(helper) Cria a função que verifica se é uma thread e se ela é a thread correta

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,1 +1,23 @@
- 
+import discord
+from src.app.config import MISSION_CHANNEL_ID
+from src.utils.embeds import create_error_embed
+
+async def is_mission_channel(interaction: discord.Interaction) -> bool:
+    """
+    Verifica se o comando está sendo usado dentro de uma Thread de Missão válida.
+    Se não estiver, já envia a mensagem de erro e retorna False.
+    """
+    # Verifica se é Thread e se o pai é o canal de missões
+    if not isinstance(interaction.channel, discord.Thread) or interaction.channel.parent_id != MISSION_CHANNEL_ID:
+        wrong_channel_embed = create_error_embed(
+            title='Você não pode usar esse comando aqui!',
+            message='Esse comando só pode ser usado dentro de uma missão'
+        )
+        if not interaction.response.is_done():
+            await interaction.response.send_message(embed=wrong_channel_embed, ephemeral=True)
+        else:
+            await interaction.followup.send(embed=wrong_channel_embed, ephemeral=True)
+
+        return False
+
+    return True
