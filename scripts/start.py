2ba94dde56029d0374f0939a22527aa556313304
Luis Gustavo Hauck <hauck.luis@gmail.com>
2026-01-19 19:40:34 -0300

    chore(add_xp_cpons) Coloca xp e moedas para os testes

--- scripts/start.py
+++ scripts/start.py
@@ -1,1 +1,15 @@
- 
+import asyncio
+from pymongo import AsyncMongoClient
+
+from src.database.connection import connect_to_database
+from src.repositories.user_repository import UserRepository
+
+
+async def start():
+    db = await connect_to_database()
+
+    item_repo = UserRepository(db)
+
+    await item_repo.add_xp_coins(user_id=593837748566097931, xp=0, coins=1000)
+
+asyncio.run(start())
