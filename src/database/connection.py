cab0bd9421059a9946735c57f1156be1a206dd11
Luis Gustavo Hauck <hauck.luis@gmail.com>
2026-02-01 18:26:59 -0300

    fix: Corrigi o erro do servidor não reconhecer a conexão com o db

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,6 +1,7 @@
 from pymongo import AsyncMongoClient
 import logging
-from src.app.config import MONGO_URI, DATABASE_NAME
+import urllib.parse
+from src.app.config import MONGO_USER, MONGO_HOST, MONGO_PASSWORD, DATABASE_NAME
 
 logger = logging.getLogger('__name__')
 
@@ -9,14 +10,30 @@ async def connect_to_database():
     Cria e retorna uma conexão com o banco de dados MongoDB usando o AsyncMongoClient
     """
     try:
-        client = AsyncMongoClient(MONGO_URI)
-        await client.admin.command('ping')
-        logger.info('Cliente criado!')
+        # Verificação de Segurança
+        if not MONGO_PASSWORD or not MONGO_HOST:
+            raise ValueError("As variáveis MONGO_PASSWORD ou MONGO_HOST estão vazias!")
 
-        # Seleciona e e retorna o abnco de dados diretamente
+        # Tratamento de Caracteres Especiais (A Correção do ==)
+        # O quote_plus transforma "==" em "%3D%3D" e arruma o "@"
+        user_safe = urllib.parse.quote_plus(MONGO_USER)
+        pass_safe = urllib.parse.quote_plus(MONGO_PASSWORD)
+
+        # 3. Montagem da URL (Padrão Cosmos DB com porta 10255)
+        mongo_uri = (
+            f"mongodb://{user_safe}:{pass_safe}@{MONGO_HOST}:10255/?"
+            "ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000"
+            f"&appName=@{MONGO_USER}@"
+        )
+
+
+        client = AsyncMongoClient(mongo_uri)
+
+        await client.admin.command('ping')
+        logger.info(f'Conectado com sucesso ao Host: {MONGO_HOST}')
+
         db = client.get_database(DATABASE_NAME)
         return db
-
     except Exception as e:
         logger.warning(f'Ocorreu um erro na criação do cleinet: {e}')
         raise
