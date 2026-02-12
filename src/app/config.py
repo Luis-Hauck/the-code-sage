2a39e41b83e27eceda28bd8e1fa88c63d6e5001b
Luis Gustavo Hauck <hauck.luis@gmail.com>
2026-02-12 19:37:56 -0300

    refactor(connection): Altera a lógica para poder receber a conexão de banco de dados diferentes

--- src/app/config.py
+++ src/app/config.py
@@ -1,15 +1,47 @@
 import os
 from dotenv import load_dotenv
-
+import urllib.parse
+import logging
 
 load_dotenv()
 
+logger = logging.getLogger('__name__')
+
+def get_mongo_uri():
+    """
+    Função que decide como montar a URI.
+    Prioridade 1: URI completa já definida (ex: Local ou Atlas).
+    Prioridade 2: Montagem manual para Cosmos DB (tratando a senha).
+    """
+    # 1. Tenta pegar a URI pronta
+    uri_env = os.getenv("MONGO_URI")
+    if uri_env:
+        return uri_env
+
+    # Se não tem URI, montamos
+    user = os.getenv("MONGO_USER")
+    password = os.getenv("MONGO_PASSWORD")
+    host = os.getenv("MONGO_HOST")
+
+    if user and password and host:
+
+        user_safe = urllib.parse.quote_plus(user)
+        pass_safe = urllib.parse.quote_plus(password)
+
+        # Monta a string específica do Cosmos DB
+        return (
+            f"mongodb://{user_safe}:{pass_safe}@{host}:10255/?"
+            "ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000"
+            f"&appName=@{user_safe}@"
+        )
+
+
+    return None
+
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD_ID = os.getenv('GUILD_ID')
-MONGO_HOST = os.getenv('MONGO_HOST')
-MONGO_PASSWORD= os.getenv('MONGO_PASSWORD')
-MONGO_USER= os.getenv('MONGO_USER')
 DATABASE_NAME = os.getenv('DATABASE_NAME')
+MONGO_URI = get_mongo_uri()
 MISSION_CHANNEL_ID = int(os.getenv('MISSION_CHANNEL_ID'))
 MOD_LOG_CHANNEL_ID = int(os.getenv('MOD_LOG_CHANNEL_ID'))
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
--- src/database/connection.py
+++ src/database/connection.py
@@ -1,7 +1,6 @@
 from pymongo import AsyncMongoClient
 import logging
-import urllib.parse
-from src.app.config import MONGO_USER, MONGO_HOST, MONGO_PASSWORD, DATABASE_NAME
+from src.app.config import DATABASE_NAME,get_mongo_uri
 
 logger = logging.getLogger('__name__')
 
@@ -10,30 +9,20 @@ async def connect_to_database():
     Cria e retorna uma conexão com o banco de dados MongoDB usando o AsyncMongoClient
     """
     try:
-        # Verificação de Segurança
-        if not MONGO_PASSWORD or not MONGO_HOST:
-            raise ValueError("As variáveis MONGO_PASSWORD ou MONGO_HOST estão vazias!")
+        mongo_uri = get_mongo_uri()
 
-        # Tratamento de Caracteres Especiais (A Correção do ==)
-        # O quote_plus transforma "==" em "%3D%3D" e arruma o "@"
-        user_safe = urllib.parse.quote_plus(MONGO_USER)
-        pass_safe = urllib.parse.quote_plus(MONGO_PASSWORD)
+        if mongo_uri:
 
-        # 3. Montagem da URL (Padrão Cosmos DB com porta 10255)
-        mongo_uri = (
-            f"mongodb://{user_safe}:{pass_safe}@{MONGO_HOST}:10255/?"
-            "ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000"
-            f"&appName=@{MONGO_USER}@"
-        )
+            client = AsyncMongoClient(mongo_uri)
 
+            await client.admin.command('ping')
+            logger.info(f'Conectado com sucesso em: {mongo_uri}')
 
-        client = AsyncMongoClient(mongo_uri)
-
-        await client.admin.command('ping')
-        logger.info(f'Conectado com sucesso ao Host: {MONGO_HOST}')
-
-        db = client.get_database(DATABASE_NAME)
-        return db
+            db = client.get_database(DATABASE_NAME)
+            return db
+        else:
+            logger.warning(f'Falha ao conectar a uri: {mongo_uri}')
+            return None
     except Exception as e:
-        logger.warning(f'Ocorreu um erro na criação do cleinet: {e}')
+        logger.error(f'Ocorreu um erro na criação do cleinet: {e}')
         raise
